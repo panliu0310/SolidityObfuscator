788acbfae8b151754ae2f3ed3da9842a0023bee3
panliu0310 <liuhoipan@gmail.com>
2025-11-27 13:36:03 +0800

    Review and correct some metrics

--- utilities/metrics.py
+++ utilities/metrics.py
@@ -3,7 +3,7 @@ import re
 
 def calculate_cyclomatic_complexity(code: str) -> int:
     # Count control structures
-    complexity = len(re.findall(r'\b(if|for|while|require|switch|case|else)\b', code))
+    complexity = len(re.findall(r'\b(if|for|while|require|switch|case)\b', code))
     return complexity
 
 def calculate_halstead_metrics(code: str):
@@ -15,34 +15,24 @@ def calculate_halstead_metrics(code: str):
     operands = re.findall(operand_pattern, code)
     
     # Count distinct operators and operands
-    n1 = len(operators)
-    n2 = len(operands)
+    N1 = len(operators)
+    N2 = len(operands)
     
     distinct_operators = set(operators)
     distinct_operands = set(operands)
     
-    N1 = len(distinct_operators)
-    N2 = len(distinct_operands)
+    n1 = len(distinct_operators)
+    n2 = len(distinct_operands)
     
     # Calculate Halstead metrics
-    L = n1 + n2
-    V = N1 + N2 # vocabulary
-    calculated_bugs = (n1 - N1 + n2 - N2) / (N1 + N2) if (N1 + N2) != 0 else 0
-    difficulty = (N1 / 2) * (n2 / N2) if N2 != 0 else 0
-    effort = difficulty * V
+    n = n1 + n2 # program vocabulary
+    N = N1 + N2 # program length
+    calculated_program_length = n1 * math.log(n1, 2) + n2 * math.log(n2, 2) if n1 > 0 and n2 > 0 else 0
+    V = N * math.log(n, 2) if n > 0 else 0 # Volume
+    D = (n1 / 2) * (N2 / n2) if n2 != 0 else 0 # Difficulty
+    E = D * V # Effort
     
-    # return {
-    #     "Distinct Operators (N1)": N1,
-    #     "Distinct Operands (N2)": N2,
-    #     "Total Operators (n1)": n1,
-    #     "Total Operands (n2)": n2,
-    #     "Vocabulary (V)": vocabulary,
-    #     "Length (L)": L,
-    #     "Calculated Bugs (E)": calculated_bugs,
-    #     "Difficulty (D)": difficulty,
-    #     "Effort (E)": effort
-    # }
-    return N1, N2, n1, n2, V
+    return n1, n2, N1, N2, calculated_program_length, V, D, E
 
 def count_lines_and_comments(code: str):
     lines = code.splitlines()
@@ -51,7 +41,7 @@ def count_lines_and_comments(code: str):
     return total_lines, comment_lines
 
 def calculate_maintainability_index(code: str):
-    N1, N2, n1, n2, V = calculate_halstead_metrics(code)
+    n1, n2, N1, N2, calculated_program_length, V, D, E = calculate_halstead_metrics(code)
     G = calculate_cyclomatic_complexity(code)
     L, comment_lines = count_lines_and_comments(code)
     
@@ -60,21 +50,23 @@ def calculate_maintainability_index(code: str):
     C_rad = C * (math.pi / 180)  # Convert percentage to radians
 
     # Calculate Maintainability Index using the given formula
-    MI = 171 - 5.2 * math.log(V) - 0.23 * G - 16.2 * math.log(L) if L > 0 and V > 0 else 0
+    MI = 171 - 5.2 * math.log(V, math.e) - 0.23 * G - 16.2 * math.log(L, math.e) + 50 * math.sin(math.sqrt(2.4 * C_rad)) if L > 0 and V > 0 else 0
 
     return MI
 
 def calculate_complexity(code: str):
     CC = calculate_cyclomatic_complexity(code)
-    N1, N2, n1, n2, V = calculate_halstead_metrics(code)
-    LOC = count_lines_and_comments(code)
+    n1, n2, N1, N2, calculated_program_length, V, D, E = calculate_halstead_metrics(code)
+    LOC, comment_lines = count_lines_and_comments(code)
     MI = calculate_maintainability_index(code)
     return f"""Cyclomatic Complexity (CC): {CC}
-Distinct Operators (N1): {N1}
-Distinct Operands (N2): {N2}
-Total Operators (n1): {n1}
-Total Operands (n2): {n2}
-Vocabulary (V): {V}
+Distinct Operators (n1): {n1}
+Distinct Operands (n2): {n2}
+Total Operators (N1): {N1}
+Total Operands (N2): {N2}
+Volume (V): {V}
+Difficulty (D): {D}
+Effort (E): {E}
 Line of code (LOC): {LOC}
 Maintainability Index (MI): {MI}\n"""
     
